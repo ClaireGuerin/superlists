2986ae2db47b0275b53f5a32973eb4a6dd58a537
ClaireGuerin <guerin.claire01@gmail.com>
2019-07-30 17:50:46 +0200

    Moved FTs into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 09:43 2019
+
+@author: claire
+"""
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+import time
+
+MAX_WAIT = 10
+
+class FunctionalTest(StaticLiveServerTestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		
+	def tearDown(self):
+		self.browser.quit()
+		
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row_text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
